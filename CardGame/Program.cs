f54812f260729b63c62e2e3d0b5ab196553519c0
archana <archana.manoj@gmail.com>
2017-08-26 00:40:11 -0700

    Card Project first commit

---
+++ CardGame/Program.cs
@@ -0,0 +1,117 @@
+﻿//Project submitted by Archana Manoj, archana.manoj@gmail.com, dated 08/25/2017
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public enum CardSuit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public class Card
+    {
+        public CardSuit CardSuit { get; set; }
+        public CardValue CardValue { get; set; }
+
+        public Card(CardSuit cardSuit, CardValue cardValue)
+        {
+            CardSuit = cardSuit;
+            CardValue = cardValue;
+        }
+    }
+
+    public class Deck
+    {
+        public List<Card> GetPack()
+        {
+            List<Card> mydeck = new List<Card>();
+            foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardValue val1 in Enum.GetValues(typeof(CardValue)))
+                {
+                    mydeck.Add(new Card(suit, val1));
+                }
+            }
+
+            return mydeck;
+        }
+
+        public List<Card> ShufflePack(List<Card> pack)
+        {
+            Random num = new Random();
+            for (int i = pack.Count - 1; i > 0; i--)
+            {
+                int n1 = num.Next(i);
+                Card temp = pack[i];
+                pack[i] = pack[n1];
+                pack[n1] = temp;
+            }
+            return pack;
+        }
+
+        public List<Card> SortPack(List<Card> pack)
+        {
+            return pack.OrderBy(x => x.CardValue).OrderBy(r => r.CardSuit).ToList();
+        }
+
+        public List<Card> DisplayPack(List<Card> pack)
+        {
+            foreach (var item in pack)
+            {
+                Console.WriteLine(item.CardValue + " of " + item.CardSuit);
+            }
+
+            return pack;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck play = new Deck();
+            List<Card> newdeck = play.GetPack();
+
+            Console.WriteLine("Get a new deck");
+
+            play.DisplayPack(newdeck);
+
+            newdeck = play.ShufflePack(newdeck);
+
+            Console.WriteLine("Shuffle the ordered deck");
+
+            play.DisplayPack(newdeck);
+
+            newdeck = play.SortPack(newdeck);
+
+            Console.WriteLine("Sort the shuffled deck");
+
+            play.DisplayPack(newdeck);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestCard/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿//Test Project submitted by Archana Manoj, archana.manoj @gmail.com, dated 08/25/2017
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCard
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestShuffle()
+        {
+            //arrange
+            List<CardGame.Card> testdeck1 = new List<CardGame.Card>();
+            List<CardGame.Card> testdeck2 = new List<CardGame.Card>();
+            CardGame.Deck trial = new CardGame.Deck();
+            testdeck1 = trial.GetPack();
+            //act
+            testdeck2 = trial.ShufflePack(trial.GetPack());
+            //assert
+            Assert.AreNotEqual(testdeck1, testdeck2);
+        }
+
+        [TestMethod]
+        public void TestSort()
+        {
+            //arrange
+            List<CardGame.Card> testdeck1 = new List<CardGame.Card>();
+            List<CardGame.Card> testdeck2 = new List<CardGame.Card>();
+            CardGame.Deck trial = new CardGame.Deck();
+            testdeck1 = trial.GetPack();
+            //act
+            testdeck2 = trial.SortPack(trial.GetPack());
+            //assert
+            for(int i = 0; i<testdeck1.Count;i++)
+            {
+                Assert.AreEqual(testdeck1[i].CardSuit, testdeck2[i].CardSuit);
+                Assert.AreEqual(testdeck1[i].CardValue, testdeck2[i].CardValue);
+            }
+           
+        }
+    }
+}
